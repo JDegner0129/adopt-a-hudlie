f869aa065fa5908f2ee14bd1a30d9c45ced2c6df
Pohl Longsine <pohl.longsine@hudl.com>
2016-03-10 16:09:01 -0600

    get user id from url path

--- src/app/profile.js
+++ src/app/profile.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import url from 'url';
+
+function getUserID() {
+  var u = url.parse(window.location.href);
+  var p = u.pathname.split("/");
+  return p[2];
+}
+
+var uid = getUserID()
+
+var ProfileTitle = React.createClass({
+  render: function() {
+    return <div>Profile of {uid}</div>;
+  }
+});
 
 ReactDOM.render(
-  <h1>Profile for: </h1>,
-  document.getElementById('profile-app')
+  <ProfileTitle/>, document.getElementById('profile-app')
 );
