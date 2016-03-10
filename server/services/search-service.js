3212fdcb1787716b624e3eef28cf484506f51164
Jordan Degner <jdegner0129@gmail.com>
2016-03-10 14:36:22 -0600

    Replace text search with regex on individual fields

--- server/services/search-service.js
+++ server/services/search-service.js
@@ -7,29 +7,38 @@ const UserCollection = collections.UserCollection;
 
 class SearchService {
   static search(query, callback) {
-    // Search interests for interests that match the query
-    InterestCollection.getInterests({ $text: { $search: query }}, (err, interests) => {
+    const interestQuery = {
+      $or: [
+        { name: { $regex: query, $options: 'i' } },
+        { 'keywords.term': { $regex: query, $options: 'i' } },
+      ],
+    };
+
+    // Search interests for interests that match the user's query
+    InterestCollection.getInterests(interestQuery, (err, interests) => {
       if (err) throw err;
 
       let interestIds = interests.map(i => i._id) || [];
 
-      let userQuery;
-      const textQuery = { $text: { $search: query } };
+      const userQuery = {
+        $or: [
+          { name: { $regex: query, $options: 'i' } },
+          { location: { $regex: query, $options: 'i' } },
+          { email: { $regex: query, $options: 'i' } },
+        ]
+      };
 
       // If any interests match up, OR the results into the query for users
       if (interestIds.length) {
-        userQuery = {
-          $or: [
-            {
-              'interests.interestId': { $in: interestIds },
-            },
-            textQuery,
-          ],
-        };
-      } else {
-        userQuery = textQuery;
+        userQuery.$or.push({
+          'interests.interestId': { $in: interestIds },
+        });
       }
 
+      // TODO: order these results by some priorities:
+      // 1. Interest rating
+      // 2. Number of keywords matched per user
+      // 3. Availability
       UserCollection.getUsers(userQuery, callback);
     });
   }
