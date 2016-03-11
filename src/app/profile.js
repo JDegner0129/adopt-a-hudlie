4aa7118da944eb2ba321f20a31b66645fee2a9a3
Pohl Longsine <pohl.longsine@hudl.com>
2016-03-10 18:38:00 -0600

    async request to fetch profile values for given userid

--- src/app/profile.js
+++ src/app/profile.js
@@ -10,12 +10,54 @@ function getUserID() {
 
 var uid = getUserID()
 
-var ProfileTitle = React.createClass({
+var UserProfile = React.createClass({
+  getInitialState: function() {
+    return {
+      userid: uid,
+      name: '',
+      email: '',
+      description: '',
+      location: '',
+      thumbnailPath: '',
+      about: ''
+    };
+  },
+
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.props.source, function (result) {
+      var userData = result[0];
+      this.setState({
+        name: user.name,
+        email: user.email,
+        description: user.description,
+        location: user.location,
+        thumbnailPath: user.thumbnailPath,
+        about: user.about
+      });
+    }.bind(this));
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
   render: function() {
-    return <div>Profile of {uid}</div>;
+    return (
+      <div>
+        <ul>
+        <li>{this.state.userid}</li>
+        <li>{this.state.name}</li>
+        <li>{this.state.email}</li>
+        <li>{this.state.description}</li>
+        <li>{this.state.location}</li>
+        <li>{this.state.thumbnailPath}</li>
+        <li>{this.state.about}</li>
+        </ul>
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <ProfileTitle/>, document.getElementById('profile-app')
+  <UserProfile source="api/v1/users/{uid}" />, document.getElementById('profile-app')
 );
